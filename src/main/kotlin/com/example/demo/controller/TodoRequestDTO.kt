9e7d2ad90ca9f1a84d1d629e20c61b8e4b3b925f
baegteun <baegteun@gmail.com>
2022-03-27 00:28:24 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/demo/controller/TodoRequestDTO.kt
@@ -0,0 +1,3 @@
+package com.example.demo.controller
+
+data class TodoRequestDTO (val todoName: String)
---
+++ src/main/kotlin/com/example/demo/repository/TodoRepository.kt
@@ -0,0 +1,7 @@
+package com.example.demo.repository
+
+import org.springframework.data.repository.CrudRepository
+
+interface TodoRepository: CrudRepository<Todo, Long> {
+
+}
